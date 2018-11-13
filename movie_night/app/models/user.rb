ceaab0559c3c87befbf1504fe0ccd839e0cb2416
AHCarl <ahcarlin@gmail.com>
2018-11-13 14:20:54 -0600

    users have password digest, password confirmation on creation

--- movie_night/app/models/user.rb
+++ movie_night/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :votes
     has_many :movies, through: :votes
     has_many :submitted_movies, :class_name => 'Movie', :foreign_key => 'submitter_id'
+    has_secure_password
 
     ROLES = %w[user admin].freeze
 end
--- movie_night/db/schema.rb
+++ movie_night/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_194153) do
+ActiveRecord::Schema.define(version: 2018_11_13_201243) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_194153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "password_digest"
   end
 
   create_table "votes", force: :cascade do |t|
